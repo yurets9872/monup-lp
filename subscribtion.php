9266beb895b6332871c11b24a1a8855e697f0e6e
Ozzy <a1@1s-MacBook-Pro.local>
2018-11-21 00:34:28 +0200

    edit 4

--- subscribtion.php
+++ subscribtion.php
@@ -4,16 +4,16 @@
     $email_exp = '/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/';
 
     $user_email = $_POST['email'];
-    $email_subject = 'User subscribtion';
-    $to = '<yurets.ua@gmail.com>';
-    $headers = 'From: $user_email' . "\r\n" .
-    'Reply-To: $user_email' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    $email_subject = "User subscribtion";
+    $to = "<yurets.ua@gmail.com>";
+    $from = "no-reply@site.com";
+    $headers = "From: <$user_email>" . "\r\n" .
+    "Reply-To: <$user_email>";
 
     if(!preg_match($email_exp, $user_email)) {
       $error_message = 'The Email Address you entered does not appear to be valid.<br />';
     }
-    $mail = mail($to, $email_subject, $headers);
+    $mail = mail($to, $email_subject, $headers, "-f " . $from);
     echo $mail;
     if ($mail) {
       echo "Thank you for using our mail form";
