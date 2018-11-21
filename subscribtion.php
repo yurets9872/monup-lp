517570a7c40be1f1230ea287d5fb661627092622
julia-lukanyuk <julia.lukanyuk@gmail.com>
2018-11-21 13:04:42 +0200

    add fix

--- subscribtion.php
+++ subscribtion.php
@@ -4,24 +4,25 @@
     $email_exp = '/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/';
 
     $user_email = $_POST['email'];
-    $subject = "User subscribtion";
-    $to = "julia.lukanyuk@gmail.com";
-    $message = 'hello';
+    $email_subject = "User subscribtion";
+    $to = "hello@monup.co";
+    $message = 'Let me know when launching';
     $headers = array(
-      "From: hello@monup.co",
-      "Reply-To: hello@monup.co",
+      "From: $user_email",
+      "Reply-To: $user_email",
       "X-Mailer: PHP/" . PHP_VERSION
     );
-
+    $headers = implode("\r\n", $headers);
+    
     if(!preg_match($email_exp, $user_email)) {
       $error_message = 'The Email Address you entered does not appear to be valid.<br />';
-    }
-    $mail = mail($to, $subject, $message, $headers);
-
-    if ($mail) {
-      echo "Thank you for using our mail form";
     } else {
-      echo "Mail sending failed.";
+      $mail = mail($to, $subject, $message, $headers);
+      if ($mail) {
+        echo "Thank you for subscribtion!";
+      } else {
+        echo "Mail sending failed.";
+      }
     }
   }
 ?>
